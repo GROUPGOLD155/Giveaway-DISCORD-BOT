15da4218bb6ce12c5222806c983a3233e68f2bed
dhiaa <scfx1024@gmail.com>
2024-12-20 00:31:32 +0100

    Update config.js

--- dhaaa/config.js
+++ dhaaa/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    token: "MTI3NTQ0NTA1Njk1ODEwMzY4Nw.GmZmgW.IayUsPU3WnlBVkZXFa-mg93mipJlcbs4-Y3NDo", // Replace with your Discord bot token
+    token: "", // Replace with your Discord bot token
     prefix: "!", // Customize your bot command prefix
-    giveawayChannelId: "1318940320699781121", // Replace with your giveaway channel ID
-    adminRoleId: "1318887620050423849", // Replace with your admin role ID
+    giveawayChannelId: "", // Replace with your giveaway channel ID
+    adminRoleId: "", // Replace with your admin role ID
   }
